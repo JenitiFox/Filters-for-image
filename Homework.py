9f769fddedb9fa057346459f3a37656260df9f9c
JenitiFox <genyakov15@gmail.com>
2026-09-22 20:26:22 +0300

    Новий коміт

--- Homework.py
+++ Homework.py
@@ -25,13 +25,8 @@ if uploaded_file is not None:
 
     filter_option = st.sidebar.selectbox("Оберіть ефект:",
                                      ["Оригінал","Чорно-Білий",
-<<<<<<< HEAD
                                       "Розмиття","Налаштування яскравості", "Налаштування контрасту",
                                       "Інверсія", "Горизонтальне відзеркалення", "Вертикальне відзеркалення"])
-=======
-                                      "Розмиття","Збільшити яскравіть",
-                                      "Інверсія"])
->>>>>>> c5fc64a9de0153e443bf6076d5d5baa94cc3cc56
     processed_img = img_array.copy()
 
     if filter_option == "Чорно-Білий":
@@ -41,7 +36,6 @@ if uploaded_file is not None:
     elif filter_option == "Розмиття":
         processed_img = cv2.GaussianBlur(img_array, (15, 15), 0)
 
-<<<<<<< HEAD
     elif filter_option == "Налаштування яскравості":
          brightness_val = st.sidebar.slider("Рівень яскравості", -100, 100, 0)
          processed_img = cv2.convertScaleAbs(img_array, alpha = 1.0, beta = brightness_val)
@@ -49,23 +43,16 @@ if uploaded_file is not None:
     elif filter_option == "Налаштування контрасту":
              contrast_val = st.sidebar.slider("Рівень контрасту", 0.5, 3.0, 1.0, step=0.1)
              processed_img = cv2.convertScaleAbs(img_array, alpha = contrast_val, beta = 0)
-=======
-    elif filter_option == "Збільшити яскравіть":
-        processed_img = cv2.convertScaleAbs(img_array, alpha = 1.0, beta = 50)
->>>>>>> c5fc64a9de0153e443bf6076d5d5baa94cc3cc56
 
     elif filter_option == "Інверсія":
         processed_img = 255 - img_array
 
-<<<<<<< HEAD
     elif filter_option == "Горизонтальне відзеркалення":
         processed_img = cv2.flip(img_array, 1)
 
     elif filter_option == "Вертикальне відзеркалення":
             processed_img = cv2.flip(img_array, 0)
 
-=======
->>>>>>> c5fc64a9de0153e443bf6076d5d5baa94cc3cc56
     with col2:
         st.subheader("Оброблене фото")
         st.image(processed_img, use_container_width = True)
